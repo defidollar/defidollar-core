89d49341e71edf1617e4cb47ee252cbff628fb84
Arpit Agarwal <93arpit@gmail.com>
2020-09-07 01:32:44 +0530

    stake in every mint, dont donate_dust (#14)
    * stake in every mint, dont donate_dust
    
    * fix test

--- contracts/peaks/curve/ICurve.sol
+++ contracts/peaks/curve/ICurve.sol
@@ -3,7 +3,7 @@ pragma solidity 0.5.17;
 interface ICurveDeposit {
     function add_liquidity(uint[4] calldata uamounts, uint min_mint_amount) external;
     function remove_liquidity_imbalance(uint[4] calldata uamounts, uint max_burn_amount) external;
-    function remove_liquidity_one_coin(uint _token_amount, int128 i, uint min_uamount, bool donate_dust) external;
+    function remove_liquidity_one_coin(uint _token_amount, int128 i, uint min_uamount) external;
     function calc_withdraw_one_coin(uint _token_amount, int128 i) external view returns(uint);
 }
 
