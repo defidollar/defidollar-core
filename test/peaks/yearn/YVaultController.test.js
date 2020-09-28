38df0dd56dfa1fe2f615393eb63eb933032bfce9
Brad Behrens <41800620+Brad-Behrens@users.noreply.github.com>
2020-09-28 13:13:52 +0100

    Update YVaultController.test.js
    - Owner tests complete
    - vaultWithdraw() tests complete (ERC20 burn error)

--- test/peaks/yearn/YVaultController.test.js
+++ test/peaks/yearn/YVaultController.test.js
@@ -4,7 +4,7 @@ const utils = require('../../utils.js')
 const toWei = web3.utils.toWei
 const fromWei = web3.utils.fromWei
 const toBN = web3.utils.toBN
-const MAX = web3.utils.toTwosComplement(-1);
+const MAX = web3.utils.toTwosComplement(-1)
 const n_coins = 4
 
 
@@ -14,6 +14,7 @@ contract('YVaultController', async (accounts) => {
         const artifacts = await utils.getArtifacts()
         Object.assign(this, artifacts)
         this.owner = accounts[0]
+        this.user = accounts[1]
 
         this.amounts = [200, 200, 200, 200] // dai, usdc, usdt, tusd
         this.reserves = this.reserves.slice(0, 3).concat([this.reserves[4]])
@@ -27,98 +28,95 @@ contract('YVaultController', async (accounts) => {
 
     // Owner tests
     describe('Only Owner', async () => {
+
         it('Owner can add Peak', async () => {
-            let reverted = false
-            try {
-                await this.controller.addPeak(this.yVaultPeak.address, {from: this.owner})
-            }
-            catch (e) {
-                reverted = true
-                assert.equal(e.reason, 'ERR_PEAK')
-            }
-            assert.equal(reverted, false, "Error: Owner could not add peak")
+            await this.controller.addPeak(this.yVaultPeak.address, {from: this.owner})
+            const result = await this.controller.peaks.call(this.yVaultPeak.address)
+            assert.equal(result, true, "Error: Owner could not add peak")
         })
- 
+        
         it('Owner can add vault', async () => {
+            await this.controller.addVault(this.yCrv.address, this.yVault.address, {from: this.owner})
+            const result = await this.controller.vaults.call(this.yCrv.address)
+            assert.equal(result, this.yVault.address, "Error: Owner could not add vault")
+        })
+
+        it('User cannot add Peak', async () => {
             let reverted = false
             try {
-                const yCRV = await this.yVaultPeak.vars()
-                await this.controller.addVault(yCRV[2], this.yVault.address, {from: this.owner})
+                await this.controller.addPeak(this.yVaultPeak.address, {from: this.user})
             }
             catch (e) {
                 reverted = true
-                assert.equal(e.reason, 'ERR_VAULT')
+                assert.equal(e.reason, 'NOT_OWNER')
             }
-            assert.equal(reverted, false, "Error: Owner could not add vault")
+            assert.equal(reverted, true)
+        })
+
+        it('User cannot add Vault', async () => {
+            let reverted = false
+            try {
+                await this.controller.addVault(this.yCrv.address, this.yVault.address, {from: this.user})
+            }
+            catch (e) {
+                reverted = true
+                assert.equal(e.reason, 'NOT_OWNER')
+            }
+            assert.equal(reverted, true)
         })
     })
 
-    // Controller token transfer to yVault test
     it('Earn', async () => {
-       /*
-       ROUTE
-
-       yVaultZap.mint()
-
-       - msg.sender [dai, usdc, usdt, tusd] => yVaultZap.address
-       - yVaultZap [dai, usdc, usdt, tusd] => yDeposit => yVaultZap.address yCRV
-       - yVaultPeak.mintWithyCRV() => yVaultPeak.address (yCRV transfer)
-       - yVaultPeak yCRV => controller.address
-       - controller.earn() => yCRV vault deposit
-       */
-       let reverted = false
-       this.amounts = [100, 100, 100, 100]
-       const tasks = []
-       for (let i = 0; i < n_coins; i++) {
-           this.amounts[i] = toBN(this.amounts[i]).mul(toBN(10 ** this.decimals[i]))
-           tasks.push(this.reserves[i].approve(this.yVaultZap.address, this.amounts[i]))
-       }
-       await Promise.all(tasks)
-       try {
-           await this.yVaultZap.mint(this.amounts, '0')
-       }
-       catch (e) {
-           reverted = true
-           assert.equal(e.reason, 'ERR_EARN')
-       }
-       // Assert statements controller yCRV => yVault
-       // All controller yCRV will be deposited into vault
-       assert.equal(fromWei(await this.yCrv.balanceOf(this.controller.address)), '0')
-       assert.equal(fromWei(await this.yVault.balanceOf(this.controller.address)), '380')
-       assert.equal(reverted, false, "Error: earn() reverted")
+        this.amounts = [100, 100, 100, 100]
+        const tasks = []
+        for (let i = 0; i < n_coins; i++) {
+            this.amounts[i] = toBN(this.amounts[i]).mul(toBN(10 ** this.decimals[i]))
+            tasks.push(this.reserves[i].approve(this.yVaultZap.address, this.amounts[i]))
+        }
+        await Promise.all(tasks)
+        await this.yVaultZap.mint(this.amounts, '0')
+        assert.equal(fromWei(await this.yCrv.balanceOf(this.controller.address)), '0')
+        assert.equal(fromWei(await this.yVault.balanceOf(this.controller.address)), '380')
     })
-
 
     // Peak tests
     describe('Only Peak', async () => {
 
-        it('Peak Vault Withdraw', async () => {
-            /*
-            ROUTE
-
-            yVaultZap.redeemInYCRV()
-
-            - Calc yCRV based on DUSD input
-            - If balance Peak > redeem safeTransfer
-            - Else controller.vaultWithdraw()
-            - vaultWithdraw() calls withdraw()
-            */
-            let reverted = false
+        it('vaultWithdraw(): peak withdraws shares', async () => {
             const dusd = toWei('100')
-            try {
-                await this.dusd.approve(this.yVaultZap.address, dusd)
-                await this.yVaultZap.redeem(dusd, [0,0,0,0])
-            }
-            catch (e) {
-                reverted = true
-                assert.equal(e.reason, 'ERR_WITHDRAW')
-            }
-            // User dusd balance - dusd amount
-            // Peak amount redeemed (All peak balance redeemed)
-            // vault amount redeemed (controller.vaultWithdraw() -> withdraw())
-            assert.equal(fromWei(await this.yCrv.balanceOf(this.yVaultPeak.address)), '0')
-            assert.equal(fromWei(await this.yVault.balanceOf(this.controller.address)), '300')
-            assert.equal(reverted, false, "Error: vaultWithdraw() reverted")
+            await this.yVaultPeak.redeemInYcrv(dusd, '0')
+            const vault_balance = await this.yVault.balanceOf(this.controller.address)
+            assert.equal(fromWei(vault_balance), '300')
+        })
+
+        it('vaultWithdraw(): peak withdraws entire vault balance', async () => {
+            const dusd = toWei('300') // Exceed == ERROR
+            await this.yVaultPeak.redeemInYcrv(dusd, '0')
+            const vault_balance = await this.yVault.balanceOf(this.controller.address)
+            assert.equal(fromWei(vault_balance), '0')
+        })
+
+        /*
+        Problem
+
+        vaultWithdraw():
+
+        1) amount = amount (amount < yCRV.balanceOf(controller))
+        2) amount = ycrv.balanceOf(controller) (amount > yCRV.balanceOf(controller))
+
+        dusd exceeds ycrv.blanceOf(controller) should withdraw entire balance
+        except *revert ERC20: burn amlunt exceeds balance*
+
+        */
+        
+        // TODO
+        it('withdraw(): peak withdraw amount from controller', async () => {
+            
+          
+        })
+
+        it('withdraw(): peak withdraw entire controller balance', async () => {
+            
         })
     })
 })
